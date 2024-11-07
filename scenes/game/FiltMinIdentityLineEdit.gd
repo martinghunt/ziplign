610b995c956abe0b7cb1f1c1d067c9260442d2b0
Martin Hunt <martinghunt@users.noreply.github.com>
2024-11-07 11:00:49 +0000

    allow pc identity filter to be float

--- scenes/game/FiltMinIdentityLineEdit.gd
+++ scenes/game/FiltMinIdentityLineEdit.gd
@@ -8,7 +8,7 @@ func _ready():
 	text = str(min_id)
 
 func _on_text_submitted(new_text):
-	if new_text.is_valid_int():
+	if new_text.is_valid_float():
 		min_id = float(new_text)
 		if min_id < 0:
 			min_id = 0
