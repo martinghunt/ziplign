b796e442679dce875cb3e7312ff993d5ea1a1b6e
Martin Hunt <martinghunt@users.noreply.github.com>
2024-11-27 14:18:21 +0000

    bug fix setting trackpad opts

--- scenes/settings/settings.gd
+++ scenes/settings/settings.gd
@@ -15,6 +15,8 @@ func _on_return_button_pressed():
 func _on_main_menu_open_settings():
 	$VBoxContainer/GridContainer/ThemeOptionButton.set_selected_to_match_theme()
 	$VBoxContainer/GridContainer/MouseWheelSensLineEdit._ready()
+	$VBoxContainer/GridContainer/TrackpadZoomSensLineEdit._ready()
+	$VBoxContainer/GridContainer/TrackpadLRSensLineEdit._ready()
 	show()
 
 func _on_theme_option_button_theme_updated():
