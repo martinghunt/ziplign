ed86cc8441f3405073fffbd0aec90d00b7417264
Martin Hunt <martinghunt@users.noreply.github.com>
2025-06-24 09:00:34 +0100

    Use zlhelper 1.0.0

--- globals.gd
+++ globals.gd
@@ -34,7 +34,7 @@ var x_zoom = 1.0
 var top_x_left = 1.0
 var bottom_x_left = 1.0
 var max_search_results = 1000
-var expect_zlhelper_version = "v0.7.0"
+var expect_zlhelper_version = "v1.0.0"
 
 
 var complement_dict = {
